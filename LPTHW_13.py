df8c3b1bbe023f70e118bc7ba82dfdd8c40dc19f
oba2311 <oba2311@gmail.com>
2019-06-26 19:38:45 +0300

    reviewing argv

---
+++ LPTHW_13.py
@@ -0,0 +1,10 @@
+from sys import argv
+
+# modify for python  3:
+
+script, first, second, third = argv
+
+print ("The script is called:"), script
+print ("Your first variable is:"), first
+print ("Your second variable is:"), second
+print ("Your third variable is:"), third
